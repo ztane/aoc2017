10826a9dc55c88ad40a3b205dbf4ca7f30445c4b
Antti Haapala <antti.haapala@anttipatterns.com>
2017-12-08 07:29:33 +0200

    even simpler code. pass registers as locals to eval, use dictionary to choose between operators.

--- days/day08.py
+++ days/day08.py
@@ -6,16 +6,11 @@ d = get_aoc_data(day=8)
 def part1_and_2():
     regs = defaultdict(int)
     absolute_maximum = 0
-    for target, op, val, cond, op2, val2 in d.parsed(
+    for tgt, op1, val, cond, op2, val2 in d.parsed(
             '<> <str:inc|dec> <int> if <> <> <int>'):
-        # __builtins__ gets injected here, so make a copy
-        if eval(f'{cond} {op2} {val2}', defaultdict(int, regs)):
-            if op == 'inc':
-                regs[target] += val
-            else:
-                assert op == 'dec'
-                regs[target] -= val
-
+        # __builtins__ gets injected into `globals`!
+        if eval(f'{cond} {op2} {val2}', {}, regs):
+            regs[tgt] = {'inc': op.add, 'dec': op.sub}[op1](regs[tgt], val)
             absolute_maximum = max(max(regs.values()), absolute_maximum)
 
     return max(regs.values()), absolute_maximum
