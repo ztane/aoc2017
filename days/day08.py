68aef7339257bd383d8e960ac1de2cf19df22dbb
Antti Haapala <antti.haapala@anttipatterns.com>
2017-12-08 07:11:58 +0200

    day 8 solution, really fugly.

--- days/day08.py
+++ days/day08.py
@@ -3,9 +3,19 @@ from helpers import *
 d = get_aoc_data(day=8)
 
 
-def part1():
-    ...
+def part1_and_2():
+    regs = defaultdict(int)
+    absolute_maximum = 0
+    for target, op, val, cond, op2, val2 in d.parsed(
+            '<> <str:inc|dec> <int> if <> <> <int>'):
+        # __builtins__ gets injected here, so make a copy
+        if eval(f'{cond} {op2} {val2}', defaultdict(int, regs)):
+            if op == 'inc':
+                regs[target] += val
+            else:
+                assert op == 'dec'
+                regs[target] -= val
 
+            absolute_maximum = max(max(regs.values()), absolute_maximum)
 
-def part2():
-    ...
+    return max(regs.values()), absolute_maximum
