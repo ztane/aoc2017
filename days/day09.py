444677e6b26d39adf7b75522fdd273ed27778751
Antti Haapala <antti.haapala@anttipatterns.com>
2017-12-09 07:12:00 +0200

    day 9.

--- days/day09.py
+++ days/day09.py
@@ -3,9 +3,36 @@ from helpers import *
 d = get_aoc_data(day=9)
 
 
-def part1():
-    ...
+def part1_and_2():
+    open_groups = []
+    total_score = 0
+    garbage = False
+    ignore = False
+    garbage_score = 0
+    for i in d.data:
+        if ignore:
+            ignore = False
+            continue
 
+        if i == '!':
+            ignore = True
+            continue
 
-def part2():
-    ...
+        if not garbage:
+            if i == '{':
+                open_groups.append(1)
+            if i == '}':
+                total_score += len(open_groups)
+                open_groups.pop()
+            if i == '<':
+                garbage = True
+                continue
+
+        else:
+            if i == '>':
+                garbage = False
+
+        if garbage:
+            garbage_score += 1
+
+    return total_score, garbage_score
