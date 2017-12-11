c265257df8b26c926d2611078d9db815b575dcfb
Antti Haapala <antti.haapala@anttipatterns.com>
2017-12-11 09:01:28 +0200

    actually, we should use abs here :P

--- days/day11.py
+++ days/day11.py
@@ -6,10 +6,10 @@ d = get_aoc_data(day=11)
 def part1_and_2():
     # http://keekerdc.com/2011/03/hexagon-grids-coordinate-systems-and-distance-calculations/
     vectors = {
-        'n': (1, 0, -1),
+        'n':  (1, 0, -1),
         'ne': (1, -1, 0),
         'se': (0, -1, 1),
-        's': (-1, 0, 1),
+        's':  (-1, 0, 1),
         'sw': (-1, 1, 0),
         'nw': (0, 1, -1)
     }
@@ -20,6 +20,6 @@ def part1_and_2():
         x += dx
         y += dy
         z += dz
-        max_d = max(max_d, x, y, z)
+        max_d = max(max_d, abs(x), abs(y), abs(z))
 
-    return max(x, y, z), max_d
+    return max(abs(x), abs(y), abs(z)), max_d
