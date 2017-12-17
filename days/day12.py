22e5cac648eb5a1af062aa67a7140981a2a46c0f
Antti Haapala <antti.haapala@anttipatterns.com>
2017-12-17 07:21:47 +0200

    day 17, bleh. Day 12 still borken.

--- days/day12.py
+++ days/day12.py
@@ -1,4 +1,6 @@
 import math
+from collections import defaultdict
+
 import numpy
 
 from helpers import get_aoc_data
@@ -10,29 +12,27 @@ def part1_and_2():
     num_lines = len(d.lines)
     adj = numpy.zeros((num_lines, num_lines))
 
-    for i in d.lines:
-        n, m = i.split(' <-> ')
+    for n, m in d.parsed('<> <<-> <str:.*>'):
         to = map(int, m.split(', '))
         n = int(n)
-        for i in to:
-            adj[n][i] = adj[i][n] = 1
-            print(n, i)
+        for line in to:
+            adj[n][line] = adj[line][n] = 1
 
-    for i in range(int(math.ceil(math.log2(n)))):
-        print(1)
+    old = adj
+    ct = 0
+    while adj.all() == old.all():
+        print(ct)
+        old = adj
         adj = (adj @ adj) > 0
 
-    multiplied = numpy.linalg.matrix_power(adj, 2000)
-    n_in_group_0 = len(list(filter(bool, multiplied[0])))
-
+    n_in_group_0 = len(list(filter(bool, adj[0])))
     seen = set()
     groups = 0
-    print(multiplied)
-    for i in range(num_lines):
-        if i not in seen:
+    for line in range(num_lines):
+        if line not in seen:
             groups += 1
             for j in range(num_lines):
-                if multiplied[i][j]:
+                if adj[line][j]:
                     seen.add(j)
 
     return n_in_group_0, groups
--- days/day17.py
+++ days/day17.py
@@ -1,11 +1,28 @@
 from helpers import *
 
 d = get_aoc_data(day=17)
+skips = int(d.data)
 
 
 def part1():
-    ...
+    spinlock = deque([0])
+
+    for i in range(1, 2017 + 1):
+        spinlock.rotate(-(skips + 1))
+        spinlock.append(i)
+        spinlock.rotate(1)
+
+    print(spinlock[spinlock.index(2017) + 1])
 
 
 def part2():
-    ...
+    spinlock = deque([0])
+
+    for i in range(1, 50_000_000 + 1):
+        spinlock.rotate(-(skips + 1))
+        spinlock.append(i)
+        spinlock.rotate(1)
+        if i % 10000 == 0:
+            print(i)
+
+    print(spinlock[spinlock.index(0) + 1])
