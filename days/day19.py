3c9abee6422098e7503ea6081c3ea7220bca88a3
Antti Haapala <antti.haapala@anttipatterns.com>
2017-12-19 07:22:45 +0200

    day 19... needs patched aocd.

--- days/day19.py
+++ days/day19.py
@@ -3,9 +3,39 @@ from helpers import *
 d = get_aoc_data(day=19)
 
 
-def part1():
-    ...
+def part1_and_2():
+    the_map = [list(i) for i in d.data.splitlines()]
+    x, y = the_map[0].index('|'), 0
 
+    if x == 0:
+        print('Warning, pretty shure you run an unpatched aocd!')
 
-def part2():
-    ...
+    dx, dy = (0, 1)
+    found = ''
+
+    ct = 0
+    while True:
+        if the_map[y][x] == ' ':
+            break
+
+        if the_map[y][x] == '+':
+            if the_map[y][x + 1] == '-':
+                dx, dy = 1, 0
+            elif the_map[y][x - 1] == '-':
+                dx, dy = -1, 0
+            elif the_map[y + 1][x] == '|':
+                dx, dy = 0, 1
+            elif the_map[y - 1][x] == '|':
+                dx, dy = 0, -1
+            else:
+                raise ValueError('dont know about turn')
+
+        elif the_map[y][x].isalpha():
+            found += the_map[y][x]
+
+        ct += 1
+        the_map[y][x] = '/'
+        y += dy
+        x += dx
+
+    return found, ct
